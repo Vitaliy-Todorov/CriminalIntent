94737b46d1b9bb5e5865421599252e18d8291702
Vitaliy_T <vitaliy.todorov0@yandex.ru>
2020-04-20 22:04:29 +0700

    Переход к тому фрагменту с расшыренной информацией об эллементах на который нажали. Отпупы от границ эерана

--- app/src/main/java/com/example/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/example/criminalintent/CrimePagerActivity.java
@@ -48,6 +48,13 @@ public class CrimePagerActivity extends AppCompatActivity {
                 return mCrimes.size();
             }
         });
+
+        for (int i = 0; i < mCrimes.size(); i++){
+            if (mCrimes.get(i).getId().equals(crimeId)){
+                mViewPager.setCurrentItem(i);                                                   //setCurrentItem - измените текущий элемент (отображаемый в ViewPager) по индексу найденного объекта
+                break;
+            }
+        }
     }
 
     public static Intent newIntent(Context packageContext, UUID crimeId){
