d79612381c5e4797ec0a70c3412b886f4edbe1c2
Vitaliy_T <vitaliy.todorov0@yandex.ru>
2020-04-29 16:02:54 +0700

    Кнопка удаления

--- app/src/main/java/com/example/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/example/criminalintent/CrimeFragment.java
@@ -53,6 +53,7 @@ public class CrimeFragment extends Fragment implements View.OnClickListener{
         //UUID crimeID = (UUID) getActivity().getIntent().getSerializableExtra(MainActivity.EXTRA_CRIME_ID);          //Это всё служет для передачи данных между активностями, в отличик от реализованной передаче межуд фрагментами.
         UUID crimeID = (UUID) getArguments().getSerializable(ARG_CRIME_ID);
         mCrime = CrimeLab.get(getActivity()).getCrime(crimeID);
+        setHasOptionsMenu(true);
     }
 
     @Override
