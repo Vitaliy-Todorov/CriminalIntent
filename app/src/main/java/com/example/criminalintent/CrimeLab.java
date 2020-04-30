aef073800d15d16869c82753c4efedd43d8f41dd
Vitaliy_T <vitaliy.todorov0@yandex.ru>
2020-04-30 21:35:37 +0700

    Создание базы данных SQL

--- app/src/main/java/com/example/criminalintent/CrimeLab.java
+++ app/src/main/java/com/example/criminalintent/CrimeLab.java
@@ -4,7 +4,10 @@ package com.example.criminalintent;
 
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
+
+import com.example.criminalintent.database.CrimeBaseHelper;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -20,6 +23,9 @@ public class CrimeLab {
     private static CrimeLab sCrimeLab;
 
     private Map<UUID, Crime> mCrimes;
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
+
 
     public void addCrime(Crime crime) {
         mCrimes.put(crime.getId(), crime);
@@ -37,6 +43,23 @@ public class CrimeLab {
     }
 
     private CrimeLab(Context context){
+        mContext = context.getApplicationContext();
+                /*getApplicationContext() - через этот метод получается доступ к singleton-экземпляру
+                (единственному на всё приложение). Этот контекст привязан к жизненному циклу
+                приложения. Контекст приложения может использоваться там, где вам нужен контекст,
+                жизненный цикл которого не связан с текущим контекстом или когда вам нужно передать
+                контекст за пределы Activity.*/
+        mDatabase = new CrimeBaseHelper(mContext).getWritableDatabase();
+                /*getWritableDatabase() класс CrimeBaseHelper выполняет следующее:
+
+                1)открывает /data/data/com.bignerdranch.android.criminalintent/databases/crimeBase.db.
+                Если файл базы данных не существует, то он создается;
+
+                2)если база данных открывается впервые, вызывает метод onCreate(SQLiteDatabase) с
+                последующим сохранением последнего номера версии;
+
+                3)если база данных открывается не впервые, проверяет номер ее версии. Если версия
+                базы данных в CrimeOpenHelper выше, то вызывается метод onUpgrade(SQLiteDatabase, int, int).*/
         mCrimes = new LinkedHashMap<>();
     }
 
--- app/src/main/java/com/example/criminalintent/database/CrimeBaseHelper.java
+++ app/src/main/java/com/example/criminalintent/database/CrimeBaseHelper.java
@@ -4,7 +4,10 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.example.criminalintent.database.CrimeDbSchema.CrimeTable;
+
 public class CrimeBaseHelper extends SQLiteOpenHelper {
+    /*код создания исходной базы данных размещается в onCreate(SQLiteDatabase), код обновления — в onUpgrade(SQLiteDatabase, int, int),*/
 
     private static final int VERSION = 1;
     private static final String DATABASE_NAME = "crimeBase.db";
@@ -15,7 +18,13 @@ public class CrimeBaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db){
-
+        db.execSQL("create table " + CrimeTable.NAME + "(" +                                    //имя таблици
+                " _id integer primary key autoincrement, " +
+                CrimeTable.Cols.UUID + ", " +
+                CrimeTable.Cols.TITLE + ", " +
+                CrimeTable.Cols.DATE + ", " +
+                CrimeTable.Cols.SOLVED +
+                ")");                                                                           //Пояснения по созданию этой строчки смотри ст 286-287. А вообще, это кода определения таблицы
     }
 
     @Override
