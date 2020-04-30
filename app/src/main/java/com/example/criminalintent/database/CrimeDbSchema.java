c8ce16c8d3d97d4dfaf62f4e4da00b408ead9d45
Vitaliy_T <vitaliy.todorov0@yandex.ru>
2020-04-30 19:46:30 +0700

    Заготовки для работы с базами данных SQL

---
+++ app/src/main/java/com/example/criminalintent/database/CrimeDbSchema.java
@@ -0,0 +1,19 @@
+//Файл CrimeDbSchema.java помещен в отдельный пакет database, который будет использоваться для организации всего кода, относящегося к базам данных.
+
+package com.example.criminalintent.database;
+
+public class CrimeDbSchema {
+
+    public static final class CrimeTable{
+
+        public static final String NAME = "crimes";
+
+        public static final class Cols{
+
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String DATE = "date";
+            public static final String SOLVED = "solved";
+        }
+    }
+}
