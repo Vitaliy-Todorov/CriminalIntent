8c1f17cfe773351dd8ce825e96d81d520d45ff0b
Vitaliy_T <vitaliy.todorov0@yandex.ru>
2020-05-07 22:28:22 +0700

    Создание двухпанельного окна

--- app/src/main/java/com/example/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/example/criminalintent/SingleFragmentActivity.java
@@ -2,6 +2,7 @@ package com.example.criminalintent;
 
 import android.os.Bundle;
 
+import androidx.annotation.LayoutRes;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -13,7 +14,8 @@ public abstract class SingleFragmentActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle saveInstanceState){
         super.onCreate(saveInstanceState);
-        setContentView(R.layout.activity_fragment);
+        //setContentView(R.layout.activity_fragment);
+        setContentView(getLayoutResId());
 
         FragmentManager fm = getSupportFragmentManager();                                       //добовляем фрагмент (наверное контейнер фрагмента). Если вы добавляете в активность несколько фрагментов, то обычно для каждого фрагмента создается отдельный контейнер со своим идентификатором.
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);                       //не понимаю зачем это, если поставить null то приложение функцианирует точно так же.
@@ -25,5 +27,10 @@ public abstract class SingleFragmentActivity extends AppCompatActivity {
                     .add(R.id.fragment_container, fragment)                                     //ст 170 FragmentTransaction, возвращают FragmentTransaction вместо void, что позволяет объединять их вызовы в цепочку. Таким образом, выделенный код в приведенном выше листинге, означает: «Создать новую транзакцию фрагмента, включить в нее одну операцию add, а затем закрепить».
                     .commit();
         }
+    }
+
+    @LayoutRes                                                                                  //@LayoutRes - Обозначает, что целочисленное возвращаемое значение параметра, поля или метода, как ожидается, будет ссылкой на ресурс макета
+    protected int getLayoutResId() {
+        return R.layout.activity_twopane;
     }
 }
